9ec5e9970256ea92b827fd23c745da6f55fd2b05
Mohammad Bayat <Ok.Bayat@hotmail.com>
2016-12-15 15:19:42 +0330

    Add files via upload

---
+++ JS/app.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict';
+
+    const elms = document.getElementsByClassName("mb-loader");
+    for (let i = 0; i < elms.length; i++) {
+        const elm = elms[i];
+        let mbCount = +elm.getAttribute("mb-count");
+        const mbSize = +elm.getAttribute("mb-size");
+
+        let divs = "";
+        if (mbSize > 0) {
+            divs = `<div  style="width: ${mbSize}px; height: ${mbSize}px">`;
+            mbCount --;
+        }
+        for (let i = 0; i < mbCount; i++) {
+            divs += "<div>";
+        }
+        for (let i = 0; i < mbCount; i++) {
+            divs += "</div>";
+        }
+        if (mbSize > 0) {
+            divs += `</div>`;
+        }
+        
+
+        elm.innerHTML = divs;
+    }
+    
+})();
